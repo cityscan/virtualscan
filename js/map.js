136448e7fd3d21b5e90b7ea08adb8d41bef0e482
Cory Mollet <cory.mollet@gmail.com>
2014-01-06 19:03:38 -0600

    populated data view with asset classes and permit status

--- js/map.js
+++ js/map.js
@@ -8,8 +8,11 @@ function init() {
     L.tileLayer('https://a.tiles.mapbox.com/v3/osaez.ggh7cflj/{z}/{x}/{y}.png', {
             attribution: 'Mapbox <a href="http://mapbox.com/about/maps" target="_blank">Terms & Feedback</a>'
             }).addTo(map);
-    var layerUrl = 'http://cityscan.cartodb.com/api/v2/viz/5bf5c460-69a4-11e3-8a42-35e8427dfec0/viz.json';
-
+    var layerUrl ='http://cityscan.cartodb.com/api/v2/viz/5bf5c460-69a4-11e3-8a42-35e8427dfec0/viz.json;'
+// old:
+// http://cityscan.cartodb.com/api/v2/viz/5bf5c460-69a4-11e3-8a42-35e8427dfec0/viz.json;
+// new
+//'http://cityscan.cartodb.com/api/v2/viz/5482c74a-771a-11e3-86e7-7310244efac4/viz.json';
     // with noun project icons
     //var layerUrl = 'http://cityscan.cartodb.com/api/v2/viz/fcc9d7a8-60fa-11e3-87c0-fb98c60814b9/viz.json';
     /*
@@ -25,7 +28,8 @@ function init() {
         .addTo(map)
         .on('done', function(layer) {
             var subLayerOptions = {
-                sql: "SELECT * FROM nyc;"
+                sql: "SELECT * FROM nyc",
+                cartocss: '#nyc{marker-fill: blue}'
                 //cartocss: '#nyc{marker-width: 24;  marker-allow-overlap: true; marker-placement: point; } {#nyc[type="Billboard"] { marker-file: url(https://s3.amazonaws.com/cityscan-nyc-demo/billboard.png);} {#nyc[type="Fence"] { marker-file: url(https://s3.amazonaws.com/cityscan-nyc-demo/fence.png);} {#nyc[type="Awning"] { marker-file: url(https://s3.amazonaws.com/cityscan-nyc-demo/signpost.png);} {#nyc[type="Scaffolding"] { marker-file: url(https://s3.amazonaws.com/cityscan-nyc-demo/construction.png);}'
                 //cartocss: "#nyc { marker-fill: #d6301d; marker-width: 16; marker-opacity: 0.9; marker-allow-overlap: true; marker-placement: point; marker-type: ellipse; marker-line-width: 2; marker-line-color: #fff; marker-line-opacity: 1;}"
             
@@ -52,6 +56,30 @@ function init() {
         expiredpermit: function() {
             sublayers[0].setSQL("SELECT * FROM nyc WHERE asviolation = 'Expired Permit'").
                 setCartoCSS("#nyc {marker-fill: yellow;}");
+            return true;
+        },
+        awning: function() {
+            sublayers[0].setSQL("SELECT * FROM nyc WHERE type = 'Awning'")
+            return true;
+        },
+        billboard: function() {
+            sublayers[0].setSQL("SELECT * FROM nyc WHERE type = 'Billboard'")
+            return true;
+        },
+        dumpster: function() {
+            sublayers[0].setSQL("SELECT * FROM nyc WHERE type = 'Dumpster'")
+            return true;
+        },
+        fence: function() {
+            sublayers[0].setSQL("SELECT * FROM nyc WHERE type = 'Fence'")
+            return true;
+        },
+        scaffold: function() {
+            sublayers[0].setSQL("SELECT * FROM nyc WHERE type = 'Scaffolding'")
+            return true;
+        },
+        sidewalkshed: function() {
+            sublayers[0].setSQL("SELECT * FROM nyc WHERE type = 'Sidewalk Shed'")
             return true;
         }
     }
